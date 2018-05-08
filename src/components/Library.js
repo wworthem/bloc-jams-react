9f5b4b61d9f19e7f03d808140d8f75eeaa13d113
Warren <warrenworthem46@gmail.com>
2018-05-08 18:30:25 -0400

    Included correct delimters for template literal

--- src/components/Library.js
+++ src/components/Library.js
@@ -13,7 +13,7 @@ class Library extends Component {
         {
           this.state.albums.map( (album, index) =>
 
-          <Link to={'/album/${album.slug}'} key={index}>
+          <Link to={`/album/${album.slug}`} key={index}>
             {album.title}
             <img src={album.albumCover} alt={album.title} />
             <div>{album.title}</div>
