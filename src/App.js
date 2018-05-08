5fe254e20f1a369ca8f5723631db8c06687b58e4
Warren <warrenworthem46@gmail.com>
2018-05-08 18:16:34 -0400

    Initial commit for library chkpt

--- src/App.js
+++ src/App.js
@@ -14,14 +14,14 @@ class App extends Component {
             <Link to='/'>Landing</Link>
             <Link to='/library'>Library</Link>
 
-            
+
           </nav>
           <h1>Bloc Jams</h1>
         </header>
         <main>
           <Route exact path="/" component={Landing} />
           <Route path="/library" component={Library} />
-          <Route path="/album" component={Album} />
+          <Route path="/album:slug" component={Album} />
         </main>
       </div>
     );
