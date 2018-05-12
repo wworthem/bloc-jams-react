6b548340b24314304804d0745d5920c1e4d80864
Warren <warrenworthem46@gmail.com>
2018-05-12 17:24:07 -0400

    initial commit of chkpt 7 audio

--- src/components/Album.js
+++ src/components/Album.js
@@ -10,9 +10,42 @@ class Album extends Component {
   });
 
   this.state = {
-    album: album
+    album: album,
+    currentSong: album.songs[0],
+    isPlaying: false
   };
+
+  this.audioElement = document.createElement('audio');
+  this.audioElement.src = album.songs[0].audioSrc;
 }
+  play() {
+    this.audioElement.play();
+    this.setState({isPlaying: true });
+  }
+
+  pause() {
+    this.audioElement.pause();
+    this.setState({isPlaying: false });
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    }
+      else {
+      if (!isSameSong) {this.setSong(song); }
+        this.play();
+      }
+    }
+
+
   render() {
     return (
       <section className="album">
@@ -32,18 +65,35 @@ class Album extends Component {
           </colgroup>
 
           <tbody>
-          <section className='songlist'>
-            {
+      {/*************************************************************
+      // My first attempt at listing the songs. Checkpoint #6...
+      //    <section className='songlist'>
+      //      {
 
-              this.state.album.songs.map( (song, index) =>
-              <tr>
-                <td>{index +1}</td>
-                <td>{song.title}</td>
-                <td>{song.duration}</td>
+      //        this.state.album.songs.map( (song, index) =>
+      //        <tr>
+      //          <td>{index +1}</td>
+      //          <td>{song.title}</td>
+      //          <td>{song.duration}</td>
+      //        </tr>
+      //        )
+      //      }
+      //    </section>
+      //**********************************************************/}
+
+            {this.state.album.songs.map( (song, index) =>
+              <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
+                <td className="song-actions">
+                  <button>
+                    <span className="song-number">{index+1}</span>
+                    <span className="ion-play"></span>
+                    <span className="ion-pause"></span>
+                  </button>
+                </td>
+                <td className="song-title">{song.title}</td>
+                <td className="song-duration">{song.duration}</td>
               </tr>
-              )
-            }
-          </section>
+            )}
 
           </tbody>
         </table>
